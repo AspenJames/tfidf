2d601d2526f9ea329a45729c1c58fc1768ec1a7d
Aspen James <hello@aspenjames.dev>
2022-05-24 19:06:25 -0600

    Add TFIDF calculation & tests

--- tfidf/corpus.go
+++ tfidf/corpus.go
@@ -1,6 +1,9 @@
 package tfidf
 
 import (
+	"fmt"
+	"math"
+
 	"github.com/google/uuid"
 )
 
@@ -32,4 +35,51 @@ func (c *Corpus) AddDocuments(ds []*Document) {
 	for _, document := range ds {
 		c.AddDocument(document)
 	}
+}
+
+// Calculates the TF-IDF value of each term in each Document of the Corpus.
+// Returns a mapping of Document ID -> mapping of term -> TF-IDF value.
+func (c *Corpus) Calculate() (map[uuid.UUID]TermTFIDF, error) {
+	for id, document := range c.corpus {
+		documentTFIDF, err := c.TFIDFs(document.GetTerms(), id)
+		if err != nil {
+			return nil, err
+		}
+		c.tfidfMap[id] = documentTFIDF
+	}
+	return c.tfidfMap, nil
+}
+
+// Calculates TF-IDF for `term` in the Document specified by `docID`.
+func (c *Corpus) TFIDF(term string, docID uuid.UUID) (TermTFIDF, error) {
+	tfidf := make(TermTFIDF)
+	doc, ok := c.corpus[docID]
+	if !ok {
+		return tfidf, fmt.Errorf("document %s not found in corpus", docID)
+	}
+	inDocCount := len(c.termInDocs[term])
+	totalDocuments := len(c.corpus)
+
+	if inDocCount == 0 {
+		tfidf[term] = 0
+	} else {
+		tf := doc.GetTF(term)
+		idf := math.Log(float64(totalDocuments) / float64(inDocCount))
+		tfidf[term] = tf * idf
+	}
+
+	return tfidf, nil
+}
+
+// Calculates TF-IDF for `terms` in the Document specified by `docID`.
+func (c *Corpus) TFIDFs(terms []string, docID uuid.UUID) (TermTFIDF, error) {
+	tfidf := make(TermTFIDF)
+	for _, term := range terms {
+		freq, err := c.TFIDF(term, docID)
+		if err != nil {
+			return tfidf, err
+		}
+		tfidf[term] = freq[term]
+	}
+	return tfidf, nil
 }
--- tfidf/corpus_test.go
+++ tfidf/corpus_test.go
@@ -1,14 +1,51 @@
 package tfidf
 
 import (
+	"math"
 	"reflect"
 	"testing"
 
 	"github.com/google/uuid"
 )
 
+// Creates a Corpus from the given term frequency maps, returns *Corpus & the
+// first Document's ID.
+func createCorpus(tfmaps []TF) (*Corpus, uuid.UUID) {
+	corpus := NewCorpus()
+	documents := []*Document{}
+	for _, tfmap := range tfmaps {
+		documents = append(documents, documentFactory(tfmap))
+	}
+	corpus.AddDocuments(documents)
+	return corpus, documents[0].ID
+}
+
 func isInCorpus(d *Document, c map[uuid.UUID]*Document) bool {
 	return c[d.ID] == d
+}
+
+func tfidfsEqual(expected, given TermTFIDF) bool {
+	if len(expected) != len(given) {
+		return false
+	}
+	for term, freq := range given {
+		if freq != expected[term] {
+			return false
+		}
+	}
+	return true
+}
+
+func tfidfMapsEqual(expected, given map[uuid.UUID]TermTFIDF) bool {
+	if len(expected) != len(given) {
+		return false
+	}
+	for id, tfidf := range given {
+		if !tfidfsEqual(expected[id], tfidf) {
+			return false
+		}
+	}
+	return true
 }
 
 func TestNewCorpus(t *testing.T) {
@@ -63,5 +100,220 @@ func TestAddDocuments(t *testing.T) {
 		if !isInCorpus(document, corpus.corpus) {
 			t.Errorf("document id %s not in corpus %v", document.ID, corpus.corpus)
 		}
+	}
+}
+
+func TestTFIDFDocumentNotFound(t *testing.T) {
+	document := documentFactory(TF{})
+	corpus := NewCorpus()
+	corpus.AddDocument(document)
+
+	garbageId := uuid.Must(uuid.NewRandom())
+	_, err := corpus.TFIDF("term", garbageId)
+	if err == nil {
+		t.Errorf("expected 'not found in corpus' error")
+	}
+}
+
+func TestTFIDF(t *testing.T) {
+	type ddt struct {
+		tfmaps   []TF
+		term     string
+		expected TermTFIDF
+	}
+	type testSuite map[string]ddt
+	suite := testSuite{
+		"termNotInCorpus": {
+			tfmaps:   []TF{{}},
+			term:     "term",
+			expected: TermTFIDF{"term": 0.0},
+		},
+		"termInAllDocuments": {
+			tfmaps: []TF{
+				{"term": 1.0},
+			},
+			term:     "term",
+			expected: TermTFIDF{"term": 0.0},
+		},
+		"termIsUnique": {
+			tfmaps: []TF{
+				{"termA": 1.0},
+				{"termB": 1.0},
+			},
+			term:     "termA",
+			expected: TermTFIDF{"termA": (1.0 * math.Log(2.0/1.0))},
+		},
+		"termIsNotUnique": {
+			tfmaps: []TF{
+				{"termA": 1.0},
+				{"termA": 1.0},
+				{"termB": 1.0},
+			},
+			term:     "termA",
+			expected: TermTFIDF{"termA": (1.0 * math.Log(3.0/2.0))},
+		},
+	}
+	for label, test := range suite {
+		t.Run(label, func(t *testing.T) {
+			corpus, targetID := createCorpus(test.tfmaps)
+
+			tfidf, err := corpus.TFIDF(test.term, targetID)
+			if err != nil {
+				t.Error(err)
+			}
+			if !tfidfsEqual(test.expected, tfidf) {
+				t.Errorf("expected %v, got %v", test.expected, tfidf)
+			}
+		})
+	}
+}
+
+func TestTFIDFs(t *testing.T) {
+	type ddt struct {
+		tfmaps   []TF
+		terms    []string
+		expected TermTFIDF
+	}
+	type testSuite map[string]ddt
+	suite := testSuite{
+		"termsNotInCorpus": {
+			tfmaps:   []TF{{}},
+			terms:    []string{"termA", "termB"},
+			expected: TermTFIDF{"termA": 0.0, "termB": 0.0},
+		},
+		"termsInCorpus": {
+			tfmaps: []TF{
+				{"termA": 0.5, "termB": 0.5},
+				{"termB": 1.0},
+			},
+			terms: []string{"termA", "termB"},
+			expected: TermTFIDF{
+				"termA": (0.5 * math.Log(2.0/1.0)),
+				"termB": (0.5 * math.Log(2.0/2.0)),
+			},
+		},
+		"mixedPresence": {
+			tfmaps: []TF{
+				{"termA": 1.0},
+				{"termB": 1.0},
+			},
+			terms: []string{"termA", "termC"},
+			expected: TermTFIDF{
+				"termA": (1.0 * math.Log(2.0/1.0)),
+				"termC": 0.0},
+		},
+	}
+	for label, test := range suite {
+		t.Run(label, func(t *testing.T) {
+			corpus, targetID := createCorpus(test.tfmaps)
+
+			tfidf, err := corpus.TFIDFs(test.terms, targetID)
+			if err != nil {
+				t.Error(err)
+			}
+			if !tfidfsEqual(test.expected, tfidf) {
+				t.Errorf("expected %v, got %v", test.expected, tfidf)
+			}
+		})
+	}
+}
+
+func TestCalculateEmptyCorpus(t *testing.T) {
+	corpus := NewCorpus()
+	tfidf, err := corpus.Calculate()
+	if err != nil {
+		t.Error(err)
+	}
+	if !tfidfMapsEqual(map[uuid.UUID]TermTFIDF{}, tfidf) {
+		t.Errorf("expected empty map, got %v", tfidf)
+	}
+}
+
+func TestCalculateSingleDocumentNoTerms(t *testing.T) {
+	corpus := NewCorpus()
+	document := documentFactory(TF{})
+	corpus.AddDocument(document)
+
+	expected := make(map[uuid.UUID]TermTFIDF)
+	expected[document.ID] = TermTFIDF{}
+
+	tfidf, err := corpus.Calculate()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !tfidfMapsEqual(expected, tfidf) {
+		t.Errorf("expected %v, got %v", expected, tfidf)
+	}
+}
+
+func TestCalculateSingleDocumentWithTerms(t *testing.T) {
+	corpus := NewCorpus()
+	document := documentFactory(TF{"term": 1.0})
+	corpus.AddDocument(document)
+
+	expected := make(map[uuid.UUID]TermTFIDF)
+	expected[document.ID] = TermTFIDF{"term": 0.0}
+
+	tfidf, err := corpus.Calculate()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !tfidfMapsEqual(expected, tfidf) {
+		t.Errorf("expected %v, got %v", expected, tfidf)
+	}
+}
+
+func TestCalculateMultipleDocumentsWithTermsSimple(t *testing.T) {
+	corpus := NewCorpus()
+	documentA := documentFactory(TF{"termA": 1.0})
+	documentB := documentFactory(TF{"termB": 1.0})
+	corpus.AddDocument(documentA)
+	corpus.AddDocument(documentB)
+
+	expected := make(map[uuid.UUID]TermTFIDF)
+	expected[documentA.ID] = TermTFIDF{"termA": 1.0 * math.Log(2.0/1.0)}
+	expected[documentB.ID] = TermTFIDF{"termB": 1.0 * math.Log(2.0/1.0)}
+
+	tfidf, err := corpus.Calculate()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !tfidfMapsEqual(expected, tfidf) {
+		t.Errorf("expected %v, got %v", expected, tfidf)
+	}
+}
+
+func TestCalculateMultipleDocumentsWithTermsComplex(t *testing.T) {
+	corpus := NewCorpus()
+	documentA := documentFactory(TF{"termA": 0.3, "termB": 0.7})
+	documentB := documentFactory(TF{"termB": 0.2, "termC": 0.8})
+	documentC := documentFactory(TF{"termD": 1.0})
+	corpus.AddDocument(documentA)
+	corpus.AddDocument(documentB)
+	corpus.AddDocument(documentC)
+
+	expected := make(map[uuid.UUID]TermTFIDF)
+	expected[documentA.ID] = TermTFIDF{
+		"termA": 0.3 * math.Log(3.0/1.0),
+		"termB": 0.7 * math.Log(3.0/2.0),
+	}
+	expected[documentB.ID] = TermTFIDF{
+		"termB": 0.2 * math.Log(3.0/2.0),
+		"termC": 0.8 * math.Log(3.0/1.0),
+	}
+	expected[documentC.ID] = TermTFIDF{
+		"termD": 1.0 * math.Log(3.0/1.0),
+	}
+
+	tfidf, err := corpus.Calculate()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !tfidfMapsEqual(expected, tfidf) {
+		t.Errorf("expected %v, got %v", expected, tfidf)
 	}
 }
